847147edc5fe807ca0feece574de3434a4cd2266
Paxnamra <bialyfeniks4@gmail.com>
2019-02-17 21:30:19 +0100

    Add model

---
+++ src/main/java/models/Book.java
@@ -0,0 +1,62 @@
+package models;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+
+public class Book {
+
+    private String title;
+    private String author;
+    private Integer numberOfPages;
+    private String isbn;
+    private BigDecimal purchasePrice;
+    private Date purchaseDate;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Integer getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    public void setNumberOfPages(Integer numberOfPages) {
+        this.numberOfPages = numberOfPages;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public BigDecimal getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public void setPurchasePrice(BigDecimal purchasePrice) {
+        this.purchasePrice = purchasePrice;
+    }
+
+    public Date getPurchaseDate() {
+        return purchaseDate;
+    }
+
+    public void setPurchaseDate(Date purchaseDate) {
+        this.purchaseDate = purchaseDate;
+    }
+}
