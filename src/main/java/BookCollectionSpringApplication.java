45adecb2fd592c9200d4fc6a765fb5f0f71f77d4
Paxnamra <bialyfeniks4@gmail.com>
2019-02-17 20:56:33 +0100

    initial

---
+++ src/main/java/BookCollectionSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.BookCollectionSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookCollectionSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookCollectionSpringApplication.class, args);
+	}
+
+}
