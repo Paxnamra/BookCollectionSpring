87a6011cf10a30797e4c730469b845f0b8d23181
Paxnamra <bialyfeniks4@gmail.com>
2019-03-02 15:29:38 +0100

    Added more controllers | changed && updated repository

--- src/main/java/controllers/BookController.java
+++ src/main/java/controllers/BookController.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,20 +18,40 @@ public class BookController {
     private BookRepository bookRepository;
 
     @GetMapping
-    public List<Book> bookList() {
+    public Iterable<Book> bookList() {
         return bookRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Book> getBook(@PathVariable("id") long id) {
+        return bookRepository.findOne(id);
+    }
+
+    @GetMapping("/{title}")
+    public List<Book> findByTitle(@PathVariable String title) {
+        return bookRepository.findByTitle(title);
+    }
+
+    @GetMapping("/{author}")
+    public List<Book> findByAuthor(@PathVariable String author) {
+        return bookRepository.findByAuthor(author);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.OK)
     public void create(@RequestBody Book book) {
         bookRepository.save(book);
-
     }
 
-    @GetMapping("/{id}")
-    public Book get(@PathVariable("id") long id) {
-        return bookRepository.getOne(id);
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") long id) {
+        bookRepository.findOne(id);
+        bookRepository.delete(id);
+    }
 
+    @PutMapping("/{id}")
+    public Book updateBook(@RequestBody Book book, @PathVariable long id) {
+        bookRepository.findOne(id);
+        return bookRepository.save(book);
     }
 }
--- src/main/java/repositories/BookRepository.java
+++ src/main/java/repositories/BookRepository.java
@@ -2,7 +2,18 @@ package repositories;
 
 import models.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface BookRepository extends JpaRepository<Book, Long> {
+import java.util.List;
+import java.util.Optional;
+
+public interface BookRepository extends CrudRepository<Book, Long> {
+
+    Optional<Book> findOne(long id);
+    List<Book> findByTitle(String title);
+    List<Book> findByAuthor(String author);
+
+    void delete(long id);
+
 
 }
